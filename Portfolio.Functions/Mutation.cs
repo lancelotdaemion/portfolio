c39c8f5e42eb9b8165bca9ed0e929d19f00c1ad6
Lance Dunnehoo <lance.dunnehoo@vision2systems.com>
2025-01-17 10:41:15 -0600

    initial setup of graphql

---
+++ Portfolio.Functions/Mutation.cs
@@ -0,0 +1,30 @@
+﻿using GraphQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Functions
+{
+    internal class Mutation
+    {
+        [GraphQLMetadata("addJedi")]
+        public Jedi AddJedi(Jedi input)
+        {
+            return StarWarsDB.AddJedi(input);
+        }
+
+        [GraphQLMetadata("updateJedi")]
+        public Jedi UpdateJedi(Jedi input)
+        {
+            return StarWarsDB.AddJedi(input);
+        }
+
+        [GraphQLMetadata("removeJedi")]
+        public string AddJedi(int id)
+        {
+            return StarWarsDB.RemoveJedi(id);
+        }
+    }
+}
